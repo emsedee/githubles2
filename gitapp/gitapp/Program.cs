e98b87c3e6b66d8402f1a1a52413ff85e6f7215e
emsedee <richardsy18@hotmail.com>
2018-10-17 21:45:36 +0200

    fiboreeks

--- gitapp/gitapp/Program.cs
+++ gitapp/gitapp/Program.cs
@@ -10,7 +10,7 @@ namespace gitapp
             int b = 478951346;
             Console.WriteLine(a + b);
             int c = 314679851;
-                int d = 255555;
+            int d = 255555;
             Console.WriteLine(c - d);
             int e = 5678;
             int k = 25556;
@@ -18,6 +18,32 @@ namespace gitapp
             int f = 100;
             int penis = 10;
             Console.WriteLine(f / penis);
+        }
+
+        public static int GetNthFibonacci_Ite(int n)
+
+        {
+
+            int number = n - 1;
+
+            int[] Fib = new int[number + 1];
+
+            Fib[0] = 0;
+
+            Fib[1] = 1;
+
+
+
+            for (int i = 2; i <= number; i++)
+
+            {
+
+                Fib[i] = Fib[i - 2] + Fib[i - 1];
+
+            }
+
+            return Fib[number];
+
         }
     }
 }
