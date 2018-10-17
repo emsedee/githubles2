3282fec597b5a0d40bb614f9145c1ab168dbb95c
KevinSnauwaert <43146887+KevinSnauwaert@users.noreply.github.com>
2018-10-17 21:39:10 +0200

    test 2

--- gitapp/gitapp/Program.cs
+++ gitapp/gitapp/Program.cs
@@ -10,14 +10,62 @@ namespace gitapp
             int b = 478951346;
             Console.WriteLine(a + b);
             int c = 314679851;
-                int d = 255555;
+            int d = 255555;
             Console.WriteLine(c - d);
             int e = 5678;
             int d = 25556;
             Console.WriteLine(e * d);
             int f = 100;
-            int penis = 10;
-            Console.WriteLine(f / penis);
+            int Richie = 10;
+            Console.WriteLine(f / Richie);
+
+            // Bereid de applicatie uit zodat deze in staat is om makkelijk een berekening te maken
+
+
+            Console.WriteLine("Geef een getal in");
+            int getal1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Geef een getal in");
+            int getal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Welke bewerking wil je doen?");
+            string value = Console.ReadLine();
+            
+            switch ("x")
+            {
+                case "x":
+                    Console.Write(getal1 + getal);
+                    
+                    break;
+                case "/":
+                    Console.Write(getal1 / getal);
+                    break;
+                case "*":
+                    Console.Write(getal1 * getal);
+                    break;
+                case "-":
+                    Console.Write(getal1 - getal);
+                    break;
+                    //do something else
+                    break;
+                default:
+                    break;
+            }
+
+
+
+
+
         }
+            
+            //if (value == + ||-||x||/)
+            //{
+                 
+            //}
+        
+
+
+        }
+
+
     }
 }
