4b33529d3265da673f966422f7bd2df748ed0c7d
emsedee <richardsy18@hotmail.com>
2018-10-17 21:09:32 +0200

    test 1

---
+++ gitapp/gitapp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace gitapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 548962134;
+            int b = 478951346;
+            Console.WriteLine(a + b);
+            int c = 314679851;
+                int d = 255555;
+            Console.WriteLine(c - d);
+            int e = 5678;
+            int d = 25556;
+            Console.WriteLine(e * d);
+            int f = 100;
+            int penis = 10;
+            Console.WriteLine(f / penis);
+        }
+    }
+}
